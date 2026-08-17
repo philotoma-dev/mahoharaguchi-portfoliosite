6a5948fe7716994c40b1929b7d3db622a55bbc63
philotoma-dev <philo.toma@gmail.com>
2026-08-17 17:58:16 +0900

    Delete js directory

--- js/common.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const navText = document.querySelector(".nav-text");
-
-hamburger.addEventListener("click", () => {
-    navText.classList.toggle("open");
-});
